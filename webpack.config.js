53ef9e76e9588696faccc786ceef9602f922f856
Neil Hemmerly <neilhemm0@gmail.com>
2023-01-30 10:20:42 -0500

    Setup webpack and package.json

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+//Webpack config for 
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/javascript/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Dayly',
+        }),
+    ],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '',
+        filename: 'bundle.js',
+        clean: true,
+    },
+
+    mode: 'development'
+}
