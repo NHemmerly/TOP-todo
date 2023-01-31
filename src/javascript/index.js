2117ad3c01cf64ae21761099182f91a5553f7cb9
Neil Hemmerly <neilhemm0@gmail.com>
2023-01-31 15:04:08 -0500

    Add projects title to DOM

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -2,8 +2,11 @@
 import '../styles/styles.css';
 import { domElement } from './factoryDOM';
 import { renderNav } from './nav';
+import { renderProjectView } from './projectsDOM';
 
+document.documentElement.classList.add('dark:bg-stone-600');
 renderNav();
+renderProjectView();
 
 
 console.log('Hi');
---
+++ src/javascript/projectsDOM.js
@@ -0,0 +1,8 @@
+import { domElement } from "./factoryDOM";
+
+export function renderProjectView() {
+    const projectView = domElement('div', {class: 'self-center text-2xl mx-3 font-semibold whitespace-nowrap text-white border-yellow-400 rounded bg-yellow-400 dark:bg-stone-700'}, 
+        domElement('span', {class: 'self-center text-2xl mx-3 font-semibold whitespace-nowrap text-white'}, 'Projects'));
+
+    document.body.appendChild(projectView);
+}
