900671aced291f0def01459b6c5bb8996b760d9b
Neil Hemmerly <neilhemm0@gmail.com>
2023-01-31 13:50:03 -0500

    Add text and add button to navbar

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -3,8 +3,6 @@ import '../styles/styles.css';
 import { domElement } from './factoryDOM';
 import { renderNav } from './nav';
 
-const tailwindTest = domElement('h1', {class: 'bg-red-900 text-white'}, 'Hello World')
-document.body.appendChild(tailwindTest);
 renderNav();
 
 
