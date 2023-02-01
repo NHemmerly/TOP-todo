37c299c305ef2a110f2edc0c4bae8b411b01cafb
Neil Hemmerly <neilhemm0@gmail.com>
2023-02-01 07:49:41 -0500

    Finish project view heading

--- src/javascript/projectsDOM.js
+++ src/javascript/projectsDOM.js
@@ -1,8 +1,8 @@
 import { domElement } from "./factoryDOM";
 
 export function renderProjectView() {
-    const projectView = domElement('div', {class: 'self-center text-2xl mx-3 font-semibold whitespace-nowrap text-white border-yellow-400 rounded bg-yellow-400 dark:bg-stone-700'}, 
-        domElement('span', {class: 'self-center text-2xl mx-3 font-semibold whitespace-nowrap text-white'}, 'Projects'));
+    const projectView = domElement('div', {class: 'flex self-center justify-center text-2xl mx-3 font-semibold whitespace-nowrap text-white border-yellow-400 rounded bg-yellow-400 dark:bg-stone-700'}, 
+        domElement('span', {class: 'self-center text-2xl my-2 font-semibold whitespace-nowrap text-white'}, 'Projects'));
 
     document.body.appendChild(projectView);
 }
