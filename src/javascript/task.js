8cccc490a6ecb78f4a36c3bf845e05c8f466d22a
Neil Hemmerly <neilhemm0@gmail.com>
2023-02-07 11:48:53 -0500

    Change border color of task cards based on prio

--- src/javascript/task.js
+++ src/javascript/task.js
@@ -2,5 +2,26 @@
 export {taskFactory};
 
 const taskFactory = (title, dueDate, priority, desc, completed) => {
+    priority = setPriorityColor(priority);
     return {title, dueDate, priority, desc, completed};
 } 
+
+function setPriorityColor(priority) {
+    switch (priority) {
+        case '1':
+            priority = 'border-l-green-500'
+            return priority;
+        case '2':
+            priority = 'border-l-yellow-200'
+            return priority;
+        case '3':
+            priority = 'border-l-yellow-400'
+            return priority;
+        case '4':
+            priority = 'border-l-red-300'
+            return priority;
+        case '5':
+            priority = 'border-l-red-600'
+            return priority;
+    }
+}
