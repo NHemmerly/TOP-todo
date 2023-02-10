9bae95cee1ce52a148ad102923fa8c0f23189b27
Neil Hemmerly <neilhemm0@gmail.com>
2023-02-10 10:51:34 -0500

    Add date validation

--- src/javascript/validate.js
+++ src/javascript/validate.js
@@ -13,7 +13,13 @@ export function validateProjectForm(input, errID, arr) {
         }
         error.classList.add('hidden') 
     }
-    return checkTitles(input, arr, errID) ? true : false;
+    if (!(checkTitles(input, arr, errID))) {
+        return false;
+    } else if(!(checkDate(input, errID))) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 function checkTitles(input, arr, errID) {
@@ -28,4 +34,52 @@ function checkTitles(input, arr, errID) {
         error.classList.add('hidden');
         return true;
     }
+}
+
+function checkDate(input, errID) {
+    const dateError = 'Please use valid date.';
+    let date = input['due-date'];
+    const error = document.getElementById(errID);
+    let datePattern = /^([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
+
+    let matchArray = date.match(datePattern);
+    if (matchArray == null) {
+        return false;
+    }
+
+    let dateString = date.replace(/\D/g, '');
+
+    let year = parseInt(dateString.substr(0, 4));
+    let month = parseInt(dateString.substr(4, 2));
+    let day = parseInt(dateString.substr(6, 2));
+
+    let daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 31];
+
+    if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)) {
+        daysInMonth[1] = 29
+    }
+    let compare = compareCurrentDate(day, month, year);
+    if (!(month < 1 || month > 12 || day < 1 || day > daysInMonth[month - 1]) && !compare) {
+        error.innerText = dateError;
+        error.classList.remove('hidden');
+        return false
+    }  
+    error.classList.add('hidden');
+    return true;
+}
+
+function compareCurrentDate(dueDay, dueMonth, dueYear) {
+    const date = new Date();
+
+    let day = parseInt(date.getDate());
+    let month = parseInt(date.getMonth() + 1);
+    let year = parseInt(date.getFullYear());
+
+    if((dueDay < day && month == dueMonth) || (dueMonth < month && year == dueYear) || (dueYear < year)) {
+        return false;
+    } else {
+        return true;
+    }
+
+    
 }
