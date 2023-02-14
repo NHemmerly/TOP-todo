df7876ac8593ae4a98d1de2d3581ed4734ee885b
Neil Hemmerly <neilhemm0@gmail.com>
2023-02-14 14:59:21 -0500

    Center info modal

--- src/javascript/projectsDOM.js
+++ src/javascript/projectsDOM.js
@@ -73,7 +73,7 @@ function getTaskObj(project, taskTitle) {
 }
 
 export function renderDefaultModal() {
-    const defaultModal = domElement('div', {id: 'defaultModal', tabindex: '-1', 'aria-hidden': 'true', class: 'fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full'},
+    const defaultModal = domElement('div', {id: 'defaultModal', tabindex: '-1', 'aria-hidden': 'true', class: 'mx-auto sm:w-3/4 md:w-2/4 fixed inset-x-0 p-4 overflow-x-hidden overflow-y-auto'},
                             domElement('div', {class: 'relative w-full h-full max-w-2xl md:h-auto'}, 
                                 domElement('div', {class: 'relative bg-white rounded-lg shadow dark:bg-gray-700'},
                                     //Task Title
